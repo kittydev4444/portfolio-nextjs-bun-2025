9407ce39d78de94b5539fa2fe7875143ba84eef7
Foursqweez <jeerasak.code@gmail.com>
2025-03-06 18:59:23 +0700

    Create Kitty-bubble random emoji

---
+++ src/components/kitty-bubble.tsx
@@ -0,0 +1,105 @@
+"use client";
+
+import { Icon } from "@iconify/react";
+import * as motion from "motion/react-client";
+import { useEffect, useState } from "react";
+import { cn } from "../utils/cn";
+
+// Define all available icons
+const allIcons = [
+  "cat-face",
+  "cat",
+  "grinning-cat-face",
+  "smiling-cat-face-with-open-mouth",
+  "grinning-cat-face-with-smiling-eyes",
+  "cat-with-tears-of-joy",
+  "smiling-cat-face-with-heart-eyes",
+  "cat-with-wry-smile",
+  "kissing-cat-face-with-closed-eyes",
+  "weary-cat-face",
+  "crying-cat-face",
+  "pouting-cat-face",
+  "black-cat",
+] as const;
+
+type Icons = (typeof allIcons)[number];
+
+type KittyBubbleProps = {
+  className?: string;
+  icon?: Icons;
+  size?: "sm" | "md" | "lg";
+  color?: string;
+  delay?: number;
+  animate?: boolean;
+};
+
+export default function KittyBubble({
+  className,
+  icon,
+  size = "md",
+  color = "bg-gradient-to-br from-kitty-pink-hot to-kitty-blue",
+  delay = 0,
+  animate = true,
+}: KittyBubbleProps) {
+  const [currentIcon, setCurrentIcon] = useState<Icons>(icon || allIcons[0]);
+
+  useEffect(() => {
+    if (icon) return;
+
+    const interval = setInterval(() => {
+      setCurrentIcon((prevIcon) => {
+        let newIcon;
+
+        do {
+          newIcon = allIcons[Math.floor(Math.random() * allIcons.length)];
+        } while (newIcon === prevIcon); // Ensure it changes
+
+        return newIcon;
+      });
+    }, 5000);
+
+    return () => clearInterval(interval); // Cleanup on unmount
+  }, [icon]);
+
+  const sizeClasses = {
+    sm: "w-12 h-12",
+    md: "w-16 h-16",
+    lg: "w-24 h-24",
+  };
+
+  return (
+    <motion.div
+      initial={{ opacity: 0, scale: 0.8 }}
+      animate={{ opacity: 1, scale: 1 }}
+      exit={{ opacity: 0, scale: 0.8 }}
+      transition={{ duration: 0.5, delay }}
+      // initial={{ y: 0 }}
+      // animate={{ y: [-10, 10, -10] }}
+      // transition={{
+      //   duration: 4,
+      //   repeat: Infinity,
+      //   repeatType: "loop",
+      //   ease: "easeInOut",
+      //   delay,
+      // }}
+      className={cn(
+        "flex items-center justify-center rounded-full p-3",
+        sizeClasses[size],
+        color,
+        animate ? "animate-bubble-float shadow-lg" : "",
+        className,
+      )}
+    >
+      <motion.div
+        key={currentIcon}
+        initial={{ opacity: 0, scale: 0.8 }}
+        animate={{ opacity: 1, scale: 1 }}
+        exit={{ opacity: 0, scale: 0.8 }}
+        transition={{ duration: 0.5 }}
+        className="h-full w-full"
+      >
+        <Icon icon={`noto:${currentIcon}`} className="h-full w-full" />
+      </motion.div>
+    </motion.div>
+  );
+}
