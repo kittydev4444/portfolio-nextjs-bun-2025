9a85c8055428483eaf2618b596f6b00f378f0096
Foursqweez <jeerasak.code@gmail.com>
2025-03-06 18:50:49 +0700

    Add local image

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    localPatterns: [
+      {
+        pathname: "/assets/images/**",
+      },
+    ],
+  },
+};
 
 module.exports = nextConfig;
