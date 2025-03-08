fee34169d6f54771a6bc5f97144de95d90b13c5c
Foursqweez <jeerasak.code@gmail.com>
2025-03-08 16:54:18 +0700

    Update card style

--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -4,6 +4,7 @@ import { Card, CardBody, CardFooter } from "@heroui/card";
 import { Chip } from "@heroui/chip";
 import { Icon } from "@iconify/react";
 import * as motion from "motion/react-client";
+import Image from "next/image";
 
 type ProjectCardProps = {
   project: Project;
@@ -22,21 +23,22 @@ export function ProjectCard({ project, index }: ProjectCardProps) {
         transition: { duration: 0.2 },
       }}
     >
-      <Card className="gradient-border h-full overflow-hidden border-none bg-white/80 shadow-lg transition-all duration-300 hover:shadow-xl dark:bg-kitty-dark/80">
-        <div className="group relative overflow-hidden">
-          <img
+      <Card
+        radius="lg"
+        className="gradient-border h-full overflow-hidden border-none bg-white/80 shadow-lg transition-all duration-300 hover:shadow-xl dark:bg-kitty-dark/80"
+      >
+        <div className="group relative">
+          <Image
             src={project.image}
             alt={project.title}
-            className="h-48 w-full object-cover transition-transform duration-500 group-hover:scale-110"
+            className="h-48 w-full bg-gradient-to-t from-black/80 to-transparent object-cover transition-transform duration-400 ease-in-out group-hover:scale-110"
+            width={1000}
+            height={1000}
           />
-          <div className="absolute inset-0 flex items-end bg-gradient-to-t from-black/80 to-transparent p-4">
-            <h3 className="font-bubblegum text-xl font-bold text-white">
-              {project.title}
-            </h3>
-          </div>
-          <div className="absolute right-2 top-2 rounded-full bg-kitty-pink-hot p-1 text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">
-            <Icon icon="lucide:star" className="h-5 w-5" />
-          </div>
+          <div className="absolute inset-0 bg-gradient-to-t from-black/80 to-transparent p-4 transition-transform duration-400 ease-in-out group-hover:scale-110" />
+          <h3 className="absolute bottom-2 left-4 font-bubblegum text-xl font-bold text-white">
+            {project.title}
+          </h3>
         </div>
         <CardBody className="relative overflow-hidden p-4">
           <div className="pointer-events-none absolute -bottom-8 -right-8 opacity-10">
