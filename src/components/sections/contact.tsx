74fcd636530c57a8fabb5b76303e12a378e96f21
Foursqweez <jeerasak.code@gmail.com>
2025-03-09 17:47:10 +0700

    Disable contact form

--- src/components/sections/contact.tsx
+++ src/components/sections/contact.tsx
@@ -1,11 +1,10 @@
-import { aboutMe, socialLinks } from "@/config/site";
+import { aboutMe, resumeLinks, socialLinks } from "@/config/site";
 import { cn } from "@/utils/cn";
 import { Card, CardBody } from "@heroui/card";
 import { Chip } from "@heroui/chip";
 import { Icon } from "@iconify/react";
 import * as motion from "motion/react-client";
 import { HTMLProps } from "react";
-import { ContactForm } from "../contact-form";
 import { HeadingSection } from "../heading-section";
 import RandomStars from "../random-star";
 
@@ -21,20 +20,57 @@ export default function Contact() {
         />
 
         <div className="grid grid-cols-1 gap-8 md:grid-cols-2">
-          <div>
-            <motion.div
-              initial={{ opacity: 0, y: 20 }}
-              whileInView={{ opacity: 1, y: 0 }}
-              transition={{ duration: 0.6 }}
-              viewport={{ once: true }}
-              className="space-y-6"
-            >
-              <p className="font-nunito text-lg text-kitty-dark/80 dark:text-kitty-light/80">
-                I&lsquo;m always open to new opportunities and collaborations.
-                Feel free to reach out if you have any questions or just want to
-                say hi! Purr~
-              </p>
+          <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.6, delay: 0.3 }}
+            viewport={{ once: true }}
+            className="space-y-6"
+          >
+            <p className="font-nunito text-lg text-kitty-dark/80 dark:text-kitty-light/80">
+              I’m currently looking for remote opportunities, particularly in
+              frontend Web3 roles. I’m always open to new opportunities and
+              collaborations. Feel free to reach out if you have any questions
+              or just want to say hi! Purr~
+            </p>
 
+            <Card className="gradient-border overflow-hidden border-none bg-kitty-dark shadow-md transition-all duration-300 hover:shadow-lg">
+              <CardBody className="p-4">
+                <h4 className="mb-3 font-bubblegum font-medium">
+                  Paw-some Skills
+                </h4>
+                <p className="mb-4 text-kitty-dark/70 dark:text-kitty-light/70">
+                  I specialize in creating purr-fect web experiences with these
+                  technologies:
+                </p>
+                <div className="flex flex-wrap gap-2">
+                  <Chip className="bg-gradient-to-r from-kitty-blue-light/20 to-kitty-pink/20">
+                    React
+                  </Chip>
+                  <Chip className="bg-gradient-to-r from-kitty-orange-pale/20 to-kitty-orange/20">
+                    Next.js
+                  </Chip>
+                  <Chip className="bg-gradient-to-r from-kitty-green-pale/20 to-kitty-green/20">
+                    TypeScript
+                  </Chip>
+                  <Chip className="bg-gradient-to-r from-kitty-pink/20 to-kitty-pink-hot/20">
+                    Tailwind CSS
+                  </Chip>
+                  <Chip className="bg-gradient-to-r from-kitty-yellow/20 to-kitty-blue-light/20">
+                    Zustand
+                  </Chip>
+                </div>
+              </CardBody>
+            </Card>
+          </motion.div>
+
+          <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.6, delay: 0.3 }}
+            viewport={{ once: true }}
+          >
+            <div className="flex flex-col gap-4">
               <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
                 <CardComponent
                   title="Email"
@@ -44,65 +80,62 @@ export default function Contact() {
                 />
                 <CardComponent
                   title="Location"
-                  des={"Cyberpunk Kitty City"}
+                  des={"Cyberpunk Kitty City (Thailand)"}
                   icon="lucide:map-pin"
                   className="from-kitty-green-pale to-kitty-orange"
                 />
               </div>
 
-              <Card className="gradient-border overflow-hidden border-none bg-kitty-dark shadow-md transition-all duration-300 hover:shadow-lg">
-                <CardBody className="p-4">
-                  <h4 className="mb-3 font-bubblegum font-medium">
-                    Paw-some Skills
+              <div className="flex gap-8">
+                <section className="space-y-2">
+                  <h4 className="font-bubblegum font-medium">
+                    Connect with me
                   </h4>
-                  <p className="mb-4 text-kitty-dark/70 dark:text-kitty-light/70">
-                    I specialize in creating purr-fect web experiences with
-                    these technologies:
-                  </p>
-                  <div className="flex flex-wrap gap-2">
-                    <Chip className="bg-gradient-to-r from-kitty-blue-light/20 to-kitty-pink/20">
-                      React
-                    </Chip>
-                    <Chip className="bg-gradient-to-r from-kitty-green-pale/20 to-kitty-green/20">
-                      TypeScript
-                    </Chip>
-                    <Chip className="bg-gradient-to-r from-kitty-orange-pale/20 to-kitty-orange/20">
-                      Next.js
-                    </Chip>
-                    <Chip className="bg-gradient-to-r from-kitty-pink/20 to-kitty-pink-hot/20">
-                      Tailwind CSS
-                    </Chip>
-                  </div>
-                </CardBody>
-              </Card>
 
-              <div>
-                <h4 className="mb-3 font-bubblegum font-medium">
-                  Connect with me
-                </h4>
-                <div className="flex gap-3">
-                  {socialLinks.map((link, index) => (
+                  <div className="flex gap-3">
+                    {socialLinks.map((link, index) => (
+                      <motion.a
+                        key={index}
+                        href={link.url}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="gradient-border overflow-hidden rounded-full bg-gradient-to-br from-kitty-blue-light/20 to-kitty-pink/20 p-3 text-kitty-dark transition-transform hover:scale-110 dark:text-kitty-light"
+                        whileHover={{ y: -5 }}
+                        initial={{ opacity: 0, scale: 0.8 }}
+                        whileInView={{ opacity: 1, scale: 1 }}
+                        transition={{ duration: 0.3, delay: index * 0.1 }}
+                        viewport={{ once: true }}
+                      >
+                        <Icon icon={link.icon} className="h-6 w-6" />
+                      </motion.a>
+                    ))}
+                  </div>
+                </section>
+                <section className="space-y-2">
+                  <h4 className="font-bubblegum font-medium">
+                    {resumeLinks.name}
+                  </h4>
+                  <div className="flex">
                     <motion.a
-                      key={index}
-                      href={link.url}
+                      href={resumeLinks.url}
                       target="_blank"
                       rel="noopener noreferrer"
                       className="gradient-border overflow-hidden rounded-full bg-gradient-to-br from-kitty-blue-light/20 to-kitty-pink/20 p-3 text-kitty-dark transition-transform hover:scale-110 dark:text-kitty-light"
                       whileHover={{ y: -5 }}
                       initial={{ opacity: 0, scale: 0.8 }}
                       whileInView={{ opacity: 1, scale: 1 }}
-                      transition={{ duration: 0.3, delay: index * 0.1 }}
+                      transition={{ duration: 0.3, delay: 0.3 }}
                       viewport={{ once: true }}
                     >
-                      <Icon icon={link.icon} className="h-6 w-6" />
+                      <Icon icon={resumeLinks.icon} className="h-6 w-6" />
                     </motion.a>
-                  ))}
-                </div>
+                  </div>
+                </section>
               </div>
-            </motion.div>
-          </div>
+            </div>
+          </motion.div>
 
-          <ContactForm />
+          {/* <ContactForm /> */}
         </div>
       </div>
     </section>
